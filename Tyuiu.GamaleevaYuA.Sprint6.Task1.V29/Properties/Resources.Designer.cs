7370d247ba15d866db78059d4b5f1eab09c9f96b
gamaleeva <gamaleevacin@mail.ru>
2025-12-03 21:10:58 +0500

    Task1

--- Tyuiu.GamaleevaYuA.Sprint6.Task1.V29/Properties/Resources.Designer.cs
+++ Tyuiu.GamaleevaYuA.Sprint6.Task1.V29/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tyuiu.GamaleevaYuA.Sprint6.Task0.V12.Properties {
+namespace Tyuiu.GamaleevaYuA.Sprint6.Task1.V29.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Tyuiu.GamaleevaYuA.Sprint6.Task0.V12.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tyuiu.GamaleevaYuA.Sprint6.Task0.V12.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tyuiu.GamaleevaYuA.Sprint6.Task1.V29.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
